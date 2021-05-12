a036eb7f675adaa465585b9e614d014a3ad7c87a
hamada456 <73261938+hamada456@users.noreply.github.com>
2021-05-12 14:07:38 +0900

    4つアラート作成

--- quiz2.js
+++ quiz2.js
@@ -1,11 +1,21 @@
-document.getElementById("js-question").innerText = "変更しました";
+document.getElementById("js-question").innerText = "問題文";
+
+
+document.getElementById("btn1").innerText = "1";
+document.getElementById("btn2").innerText = "2";
+document.getElementById("btn3").innerText = "3";
+document.getElementById("btn4").innerText = "4";
 
 
 document.getElementById("btn1").onclick = ()=>{
 	window.alert("1です");
 }
-
-
-document.getElementById("btn2").innerText = "2";
-document.getElementById("btn3").innerText = "3";
-document.getElementById("btn4").innerText = "4";
+document.getElementById("btn2").onclick = ()=>{
+	window.alert("2です");
+}
+document.getElementById("btn3").onclick = ()=>{
+	window.alert("3です");
+}
+document.getElementById("btn4").onclick = ()=>{
+	window.alert("4です");
+}
