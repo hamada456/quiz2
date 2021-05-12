f517afebab43b205ece8ecb207ed704eae6ff9a8
hamada456 <73261938+hamada456@users.noreply.github.com>
2021-05-12 15:09:22 +0900

    答えを配列定義代入して書き換え

--- quiz2.js
+++ quiz2.js
@@ -10,21 +10,8 @@ const answers = [
 	"D"
 ];
 
-document.getElementById("btn1").innerText = "1";
-document.getElementById("btn2").innerText = "2";
-document.getElementById("btn3").innerText = "3";
-document.getElementById("btn4").innerText = "4";
+document.getElementById("btn0").innerText = answers[0];
+document.getElementById("btn1").innerText = answers[1];
+document.getElementById("btn2").innerText = answers[2];
+document.getElementById("btn3").innerText = answers[3];
 
-
-document.getElementById("btn1").onclick = ()=>{
-	window.alert("1です");
-}
-document.getElementById("btn2").onclick = ()=>{
-	window.alert("2です");
-}
-document.getElementById("btn3").onclick = ()=>{
-	window.alert("3です");
-}
-document.getElementById("btn4").onclick = ()=>{
-	window.alert("4です");
-}
