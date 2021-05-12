c494c4e4e2df08ec04650b387db33283b25d939e
hamada456 <73261938+hamada456@users.noreply.github.com>
2021-05-12 13:56:24 +0900

    クリックでアラートを出す

--- quiz2.js
+++ quiz2.js
@@ -1,4 +1,11 @@
-
 document.getElementById("js-question").innerText = "変更しました";
 
-document.getElementById("btn1").innerText = "変更しました";
+
+document.getElementById("btn1").onclick = ()=>{
+	window.alert("1です");
+}
+
+
+document.getElementById("btn2").innerText = "2";
+document.getElementById("btn3").innerText = "3";
+document.getElementById("btn4").innerText = "4";
