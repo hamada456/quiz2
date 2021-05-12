7888fa8ee3c835528c7244c40a016ea52f88d090
hamada456 <73261938+hamada456@users.noreply.github.com>
2021-05-12 16:31:04 +0900

    if文で正誤判定（修正）

--- quiz2.js
+++ quiz2.js
@@ -1,23 +1,46 @@
 //問題文
 const question = 'Aは？';
 document.getElementById("js-question").innerText = question;
-//回答一覧
+//答え
+const correct = "A";
+//回答一覧の代入
 const answers = [
 	"A",
 	"B",
 	"C",
 	"D"
 ];
-//答え
-const correct = "A";
-//回答一覧の代入
 document.getElementById("btn0").innerText = answers[0];
 document.getElementById("btn1").innerText = answers[1];
 document.getElementById("btn2").innerText = answers[2];
 document.getElementById("btn3").innerText = answers[3];
 //クリックイベントと正誤判定のif文
-document.getElementsByTagName("button")[0].addEventListener("click",() => {
-if (){
+document.getElementById("btn0").addEventListener("click",() => {
+if (correct === document.getElementById("btn0").innerText){
+	window.alert("正解");
+}else{
+	window.alert("不正解");
+	}
+});
+
+document.getElementById("btn1").addEventListener("click",() => {
+if (correct === document.getElementById("btn1").innerText){
+	window.alert("正解");
+}else{
+	window.alert("不正解");
+	}
+});
+
+document.getElementById("btn2").addEventListener("click",() => {
+if (correct === document.getElementById("btn2").innerText){
+	window.alert("正解");
+}else{
+	window.alert("不正解");
+	}
+});
+
+document.getElementById("btn3").addEventListener("click",() => {
+if (correct === document.getElementById("btn3").innerText){
 	window.alert("正解");
 }else{
 	window.alert("不正解");
