776d1768157052d86613b9cb43e0f96b0191f321
hamada456 <73261938+hamada456@users.noreply.github.com>
2021-05-12 15:46:46 +0900

    if文で正誤判定、回答とクリックの文が一緒か？

--- quiz2.js
+++ quiz2.js
@@ -17,3 +17,9 @@ document.getElementById("btn1").innerText = answers[1];
 document.getElementById("btn2").innerText = answers[2];
 document.getElementById("btn3").innerText = answers[3];
 
+
+if (correct === document.getElementById("btn0").innerText){
+	window.alert("正解");
+}else{
+	window.alert("不正解");
+};
