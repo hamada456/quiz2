a7b82629d2b0261f0b5b12d5aef4438ca9790715
hamada456 <73261938+hamada456@users.noreply.github.com>
2021-05-13 11:19:05 +0900

    冗長なコードを定数代入

--- quiz2.js
+++ quiz2.js
@@ -10,37 +10,44 @@ const answers = [
 	"C",
 	"D"
 ];
-document.getElementById("btn0").innerText = answers[0];
-document.getElementById("btn1").innerText = answers[1];
-document.getElementById("btn2").innerText = answers[2];
-document.getElementById("btn3").innerText = answers[3];
+
+const $button0 = document.getElementById("btn0");
+const $button1 = document.getElementById("btn1");
+const $button2 = document.getElementById("btn2");
+const $button3 = document.getElementById("btn3");
+
+$button0.innerText = answers[0];
+$button1.innerText = answers[1];
+$button2.innerText = answers[2];
+$button3.innerText = answers[3];
+
 //クリックイベントと正誤判定のif文
-document.getElementById("btn0").addEventListener("click",() => {
-if (correct === document.getElementById("btn0").innerText){
+$button0.addEventListener("click",() => {
+if (correct === $button0.innerText){
 	window.alert("正解");
 }else{
 	window.alert("不正解");
 	}
 });
 
-document.getElementById("btn1").addEventListener("click",() => {
-if (correct === document.getElementById("btn1").innerText){
+$button1.addEventListener("click",() => {
+if (correct === $button1.innerText){
 	window.alert("正解");
 }else{
 	window.alert("不正解");
 	}
 });
 
-document.getElementById("btn2").addEventListener("click",() => {
-if (correct === document.getElementById("btn2").innerText){
+$button2.addEventListener("click",() => {
+if (correct === $button2.innerText){
 	window.alert("正解");
 }else{
 	window.alert("不正解");
 	}
 });
 
-document.getElementById("btn3").addEventListener("click",() => {
-if (correct === document.getElementById("btn3").innerText){
+$button3.addEventListener("click",() => {
+if (correct === $button3.innerText){
 	window.alert("正解");
 }else{
 	window.alert("不正解");
