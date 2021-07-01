52df1cc3c9e5ff0bf776b2959d58a5aafa2c2b26
hamada456 <73261938+hamada456@users.noreply.github.com>
2021-07-01 15:25:10 +0900

    07011525

--- start2.js
+++ start2.js
@@ -1,9 +1,9 @@
 let userName = prompt("名前は？","コアシスタロウ");
 
 if(userName === null || userName === ""){//キャンセルはnullを返す
-  window.alert("達者でね！");
+  window.alert("名前無し、キャンセルは失格です");
   location.href = "https://www.yahoo.co.jp/";//ヤフーへ飛ばす
-  throw new Error("名無しは失格です");//処理を終了
+  throw new Error("デバッグA");//処理を終了
 };
 
 let okNg = window.confirm( "「" + userName + "」\nこの名前でいいですか？");//option + ¥でバックスラッシュ
@@ -11,7 +11,7 @@ let okNg = window.confirm( "「" + userName + "」\nこの名前でいいです�
 if(okNg){
   window.alert("問題スタート！！");
 }else{
-  window.alert("達者でね！");
-  location.href = "https://www.yahoo.co.jp/";
-  throw new Error("名無しは失格です");
+  window.alert("考え直す時間はありません");
+  location.href = "https://www.google.com/";
+  throw new Error("デバッグB");
 };
