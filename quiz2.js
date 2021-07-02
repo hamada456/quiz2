e09d39fe73a88436f0d88d099e07a53c47312f29
hamada456 <73261938+hamada456@users.noreply.github.com>
2021-07-02 13:28:54 +0900

    07021330

--- quiz2.js
+++ quiz2.js
@@ -19,46 +19,51 @@ const quiz = [
     answers :["A","B","C","D"]
   }];
 
+const $button = document.getElementsByTagName("button");
+//クイズ１問
+const setupQuiz = () => {
 //問題文の代入
 document.getElementById("js-question").innerText = quiz[0].question;
-
 //回答一覧の代入
-const $button = document.getElementsByTagName("button");
-
 let buttonIndex = 0;
 let buttonLength = $button.length;
 while(buttonIndex < buttonLength){
 	$button[buttonIndex].innerText = quiz[0].answers[buttonIndex];
 	buttonIndex++;
 }
+}
+setupQuiz();
 
+const clickHandler = (e) => {
+if (quiz[0].correct === e.target.innerText){
+	window.alert("正解");
+}else{
+	window.alert("不正解");
+}
+};
 //クリックイベントと正誤判定のif文
-$button[0].addEventListener("click",() => {
-if (quiz[0].correct === $button[0].innerText){
+$button[0].addEventListener("click",(e) => {
+	clickHandler(e);
+});
+
+$button[1].addEventListener("click",(e) => {
+if (quiz[0].correct === e.target.innerText){
   window.alert("正解");
 }else{
   window.alert("不正解");
   }
 });
 
-$button[1].addEventListener("click",() => {
-if (quiz[0].correct === $button[1].innerText){
+$button[2].addEventListener("click",(e) => {
+if (quiz[0].correct === e.target.innerText){
   window.alert("正解");
 }else{
   window.alert("不正解");
   }
 });
 
-$button[2].addEventListener("click",() => {
-if (quiz[0].correct === $button[2].innerText){
-  window.alert("正解");
-}else{
-  window.alert("不正解");
-  }
-});
-
-$button[3].addEventListener("click",() => {
-if (quiz[0].correct === $button[3].innerText){
+$button[3].addEventListener("click",(e) => {
+if (quiz[0].correct === e.target.innerText){
   window.alert("正解");
 }else{
   window.alert("不正解");
